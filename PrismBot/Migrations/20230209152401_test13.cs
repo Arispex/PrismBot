89c8f7c4afb3cda8e693924431592bda85f7fb05
Qianyiovo <qianyiovo@outlook.com>
2023-02-09 23:39:23 +0800

    更新Migrations

---
+++ PrismBot/Migrations/20230209152401_test13.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrismBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class test13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFreeze",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSignedIn",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationTime",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFreeze",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "IsSignedIn",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationTime",
+                table: "Players");
+        }
+    }
+}
