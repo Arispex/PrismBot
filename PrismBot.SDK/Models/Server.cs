520eb9231af5564a5e4596ccb4e206719b7999c1
Qianyiovo <qianyiovo@outlook.com>
2023-02-10 23:08:59 +0800

    优化代码

--- PrismBot.SDK/Models/Server.cs
+++ PrismBot.SDK/Models/Server.cs
@@ -1,5 +1,8 @@
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 using System.Text.Json;
+using System.Web;
+using PrismBot.SDK.Exceptions;
 
 namespace PrismBot.SDK.Models;
 
@@ -22,15 +25,43 @@ public class Server
     }
     
     private Server() {}
-    
-    public async Task<string[]> GetActiveListAsync()
+
+    public async Task<Dictionary<string, string>> SendGetToEndpointAsync(string endpointPath,
+        Dictionary<string, object> @params)
     {
+        var query = HttpUtility.ParseQueryString(String.Empty);
+        foreach (var key in @params.Keys)
+        {
+            foreach (var value in @params.Values)
+            {
+                query[key] = value.ToString();
+            }
+        }
         using var httpClient = new HttpClient();
-        var response = await httpClient.GetAsync($"http://{Host}:{Port}/v2/users/activelist?token={Token}");
+        var response = await httpClient.GetAsync($"http://{Host}:{Port}/{endpointPath}?{query}");
+        switch (response.StatusCode)
+        {
+            case HttpStatusCode.Forbidden:
+                throw new InvalidToken("提供了无效的Token");
+            case HttpStatusCode.Unauthorized:
+                throw new NotAuthorized("API需要Token");
+            case HttpStatusCode.BadRequest:
+                throw new MissingParameters("缺少参数");
+        }
+
         response.EnsureSuccessStatusCode();
         var result =
-            await JsonSerializer.DeserializeAsync<Dictionary<string, String>>(
+            await JsonSerializer.DeserializeAsync<Dictionary<string, string>>(
                 await response.Content.ReadAsStreamAsync());
+        return result;
+    }
+
+    public async Task<string[]> GetActiveListAsync()
+    {
+        var result = await SendGetToEndpointAsync("v2/users/activelist", new Dictionary<string, object>
+        {
+            {"token", Token}
+        });
         var activeUsers = result["activeusers"];
         if (activeUsers.Length == 0) return Array.Empty<String>();
         return activeUsers.Split("\t");
