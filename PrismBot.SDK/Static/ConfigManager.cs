186301550d054ae88112009ae38b969002bda6cc
Qianyiovo <qianyiovo@outlook.com>
2023-02-20 16:20:30 +0800

    新增 SaveConfig()

--- PrismBot.SDK/Static/ConfigManager.cs
+++ PrismBot.SDK/Static/ConfigManager.cs
@@ -15,11 +15,18 @@ public static class ConfigManager
         var configPath = Path.Combine(Environment.CurrentDirectory, "configs", fileName);
         if (!File.Exists(configPath)) throw new FileNotFoundException($"找不到配置文件 {fileName}");
 
-        using (var streamReader = new StreamReader(configPath))
-        {
-            var deserializer = new Deserializer();
-            return deserializer.Deserialize<T>(streamReader);
-        }
+        using var streamReader = new StreamReader(configPath);
+        var deserializer = new Deserializer();
+        return deserializer.Deserialize<T>(streamReader);
+    }
+
+    public static void SaveConfig(string fileName, object obj)
+    {
+        var configPath = Path.Combine(Environment.CurrentDirectory, "configs", fileName);
+        
+        using var streamWriter = new StreamWriter(configPath);
+        var serializer = new Serializer();
+        serializer.Serialize(streamWriter, obj);
     }
 
     /// <summary>
